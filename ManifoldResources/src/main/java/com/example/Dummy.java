9ccbd71376fc32fbce2ab5341c3ebbc7fa95d75e
scott <rsmckinney@hotmail.com>
2020-07-13 12:40:45 -0700

    minor

--- ManifoldResources/src/main/java/com/example/Dummy.java
+++ ManifoldResources/src/main/java/com/example/Dummy.java
@@ -1,13 +1,5 @@
 package com.example;
 
-import com.example.schema.movies.*;
-import com.example.schema.queries.MovieQuery;
-
-import java.util.List;
-
-import static com.example.schema.movies.Genre.*;
-import static java.lang.System.out;
-
 /**
  * At least one Java source file must exist in order to enable Java compilation of resources.
  */
